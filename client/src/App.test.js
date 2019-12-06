44a00d78ca24a84eb1d2e375e17e07b9cb5ae46a
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2019-12-06 10:16:37 -0800

    Its Working

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Display from './components/Display';
+import DataFetch from './components/DataFetch';
+import { render, fireEvent } from '@testing-library/react'
 
-it('renders without crashing', () => {
+test('renders without crashing Display', () => {
+render(<Display/>);
+});
+
+test('renders without crashing DataFetch', () => {
+render(<DataFetch/>);
+});
+
+test('render Womens World Cup', () => {
+  const wrapper = rtl.render(
+    <span className="greet">Women's World Cup</span>
+  );
+  // the querying functionality is accessed through the wrapper:
+  const element = wrapper.queryByText(/WomensWorldCup/i);
+
+  // assertions will come in here
+});
+
+test('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
